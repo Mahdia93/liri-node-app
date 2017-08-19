6c59fb3ca19d0bf822763d9d441388084852ba9d
Mahdia Hashimy <mahdia@Mahdias-MBP.fios-router.home>
2017-08-19 13:59:03 -0400

    keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '5SolFW2isZPN1SBKVDrrilS61',
+  consumer_secret: '	9h7tiW4SJeWMi3j9wSTfj2FOlC20d1smPTPPz3dfLh9S6DTTED',
+  access_token_key: '898617913485033472-NflAVtv0OS28yGkiqxOCQl2lpEuOXXQ',
+  access_token_secret: 'ODeVytaSRfbgCYZdqcHwdL0synsmpR4vvSJ1JXLEQAKyd',
+}
